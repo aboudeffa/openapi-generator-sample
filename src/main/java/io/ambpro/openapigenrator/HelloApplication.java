20072a2347fc7acb1807b58710eecdfcfbe07631
Amin Boudeffa <amin.boudeffa@ext.adeo.com>
2023-10-08 17:32:53 +0200

    init project

---
+++ src/main/java/io/ambpro/openapigenrator/HelloApplication.java
@@ -0,0 +1,13 @@
+package io.ambpro.openapigenrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HelloApplication.class, args);
+  }
+
+}
